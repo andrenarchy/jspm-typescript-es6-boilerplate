b33b75cb293152b64750b24c5edb88904d7d1ef8
André Gaul <andre@gaul.io>
2016-02-01 17:44:24 +0100

    typescript es5

---
+++ jspm.config.js
@@ -0,0 +1,46 @@
+SystemJS.config({
+  packageConfigPaths: [
+    "github:*/*.json",
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  globalEvaluationScope: false,
+  transpiler: "plugin-typescript",
+
+  map: {
+    "angular": "github:angular/bower-angular@1.4.9",
+    "assert": "github:jspm/nodelibs-assert@0.2.0-alpha",
+    "paperhive-sources": "npm:paperhive-sources@4.0.1",
+    "pdfjs": "github:mozilla/pdfjs-dist@1.4.37",
+    "plugin-typescript": "github:frankwallis/plugin-typescript@2.5.8",
+    "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+    "ts": "github:frankwallis/plugin-typescript@2.5.8",
+    "url": "github:jspm/nodelibs-url@0.2.0-alpha"
+  },
+
+  packages: {
+    "src": {
+      "meta": {
+        "*.ts": {
+          "loader": "ts"
+        }
+      }
+    },
+    "github:jspm/nodelibs-url@0.2.0-alpha": {
+      "map": {
+        "url-browserify": "npm:url@0.11.0"
+      }
+    },
+    "npm:url@0.11.0": {
+      "map": {
+        "punycode": "npm:punycode@1.3.2",
+        "querystring": "npm:querystring@0.2.0"
+      }
+    },
+    "github:frankwallis/plugin-typescript@2.5.8": {
+      "map": {
+        "typescript": "npm:typescript@1.7.5"
+      }
+    }
+  }
+});
